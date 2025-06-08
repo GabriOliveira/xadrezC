f5ad4aa984474c2ddffcb601829c25ba443aba49
GabriOliveira <gabriel007og@gmail.com>
2025-06-08 14:55:33 -0300

    Feito ajustes e melhorias em cada recursividade das peças, substituição de if para switch na escolha das peças

--- main.c
+++ main.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+// Movimento das peças (quantidade = quantidade de movimentos e unidade = ações que teram e cada quantidade de movimento)
+void bispoMove(int quantidade1)
+{ // Movimento do Bispo com recursividade
+    for (int unidade1 = 1; unidade1 <= quantidade1; unidade1++)
+    {
+        printf("Cima \n");
+        printf("Direita \n");
+        printf("FIM DO MOVIMENTO %d \n", unidade1);
+    }
+}
+
+void torreMove(int quantidade2)
+{ // Movimento da Torre com recursividade
+    for (int unidade2 = 1; unidade2 <= quantidade2; unidade2++)
+    {
+        printf("Direita  \n");
+        printf("FIM DO MOVIMENTO %d \n", unidade2);
+    }
+}
+
+void rainhaMove(int quantidade3)
+{ // Movimento da Rainha com recursividade
+    for (int unidade3 = 1; unidade3 <= quantidade3; unidade3++)
+    {
+        printf("Esquerda \n");
+        printf("Cima \n");
+        printf("Direita \n");
+        printf("FIM DO MOVIMENTO %d \n", unidade3);
+    }
+}
+
+void cavaloMove(int quantidade4)
+{ // Movimento do Cavalo com recursividade
+    for (int unidade4 = 1; unidade4 <= quantidade4; unidade4++)
+    {
+        for (int cima = 1; cima <= 2; cima++)
+        {
+            printf("Cima \n");
+        }
+        for (int direita = 1; direita <= 1; direita++)
+        {
+            printf("Direita \n");
+
+            printf("FIM DO MOVIMENTO %d \n", unidade4);
+        }
+    }
+}
 
 int main()
 {
     int peca;
-
+    int quantidade_movimento;
+    // Entrada de Dados para escolha da peça
     printf("Selecione a peça para usar no xadrez: \n");
     printf("1 para Bispo \n");
     printf("2 para Torre \n");
@@ -11,51 +59,35 @@ int main()
     printf("4 para Cavalo \n");
     scanf("%d", &peca);
 
-    // Bispo(While)
-    if (peca == 1)
-    {
-        int b = 1;
-        while (b <= 5)
-        {
-            printf("Cima, Direita \n");
-            b++;
-        }
-    }
+    printf("Qual a quantidade de movimentos? \n");
+    scanf("%d", &quantidade_movimento);
 
-    // Torre(Do while)
-    if (peca == 2)
+    // Estrutura de decisão por trás da escolha da peça, e chamada recursividade
+    switch (peca)
     {
-        int t = 1;
-        do
-        {
-            printf("Direita \n");
-            t++;
-        } while (t <= 5);
-    }
+    // Bispo
+    case 1:
+        bispoMove(quantidade_movimento);
+        break;
 
-    // Rainha(For)
-    if (peca == 3)
-    {
-        for (int r = 1; r <= 8; r++)
-        {
-            printf("Esquerda \n");
-        }
-    }
+    // Torre
+    case 2:
+        torreMove(quantidade_movimento);
+        break;
 
-    // Cavalo(Loops aninhados)
-    if (peca == 4)
-    {
-        int v = 1;
-        for (int c = 1; c <= 1; c++)
-        {
-            do
-            {
-                printf("Baixo\n");
-                v++;
-            } while (v == 2);
+    // Rainha
+    case 3:
+        rainhaMove(quantidade_movimento); // 8
+        break;
 
-            printf("Esquerda\n");
-        }
+    // Cavalo
+    case 4:
+        cavaloMove(quantidade_movimento);
+        break;
+
+    default:
+        printf("Peça Digitada Inválida..:/ \n");
+        break;
     }
 
     return 0;
